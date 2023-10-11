c2887bae24e8264fa185a54e9281134ebba00f10
antoninmansour <antoninmansour@ANTONINMANSE56C>
2023-10-11 09:40:53 +0200

    Vecteur2D

---
+++ SpaceInvaders/SpaceShip.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    internal class SpaceShip
+    {
+        private double speedPixelPerSecond;
+        
+    }
+}
